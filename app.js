0a69110b8793c27581f51cc2e218dfac7a6a8dc0
vasudevreddy33 <vasudev.reddy3@gmail.com>
2017-04-23 20:15:10 -0400

    added app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var routes = require('./routes');
+var path = require('path');
+
+var app = express();
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes
+
+//home
+app.get('/', routes.home);
+
+//movie_single
+app.get("/star_wars/:episode_number?", routes.movies_single);
+
+//404 not found
+
+app.get('*', routes.not_found);
+
+app.listen(3000, function(){
+	console.log("Server Started");
+});
