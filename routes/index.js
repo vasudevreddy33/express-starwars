bab26256be17cc9fe22079263f5a5f754d9bd251
vasudevreddy33 <vasudev.reddy3@gmail.com>
2017-04-24 20:41:33 -0400

    added all views and completed routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var moviesJSON = require('../movies.json')
 
-//home
+//home (home.ejs in the views folder)
 exports.home = function(req, res){
 	res.render('home', {
 		title: 'Star Wars Movies',
@@ -8,13 +8,36 @@ exports.home = function(req, res){
 	});
 };
 
-//movie single
+//movies_single (looks for movies_single.ejs in the views folder)
 exports.movies_single = function(req, res){
+	//sets episode number for each epidode link
 	var episode_number = req.params.episode_number;
-	res.send("You are watching episode" + episode_number);
+
+	if(episode_number>=1 && episode_number<=6){
+		var current_episode = moviesJSON.movies[episode_number - 1];
+	
+		//response will render movies single link
+		res.render('movies_single', {
+		movies: moviesJSON.movies,
+		title: current_episode.title,
+		poster: current_episode.poster,
+		description: current_episode.description,
+		main_characters: current_episode.main_characters,
+		movie_bg: current_episode.hero_image
+	});
+	}
+	else{
+		res.render('not_found', {
+			movies: moviesJSON.movies,
+			title: "Movie Unavailable"
+		});
+	}
 };
 
 //not found
 exports.not_found = function(req, res){
-	res.send("Page not available");
+	res.render('not_found', {
+			movies: moviesJSON.movies,
+			title: "Page Unavailable"
+		});
 };
